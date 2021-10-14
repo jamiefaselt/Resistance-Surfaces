6ce6f96136eb353bfc10b9e7db79cd9d0ec156dc
Jamie Faselt <jamiefaselt@u.boisestate.edu>
2021-10-14 09:45:33 -0600

    new scripts in progress

---
+++ datasets to spatialize.R
@@ -0,0 +1,13 @@
+# URL=https://quickstats.nass.usda.gov/results/297D4450-C108-3D63-A4C6-AD4C06A2410C
+# CATTLE, COWS, BEEF - INVENTORY
+# NASS
+
+# URL = URL=https://quickstats.nass.usda.gov/results/A38481AD-2F20-3BF8-9295-653190550348
+# ag land value $ 
+
+
+# Montana Cadastral Data
+# https://ftpgeoinfo.msl.mt.gov/Data/Spatial/MSDI/Cadastral/
+# MontanaCadastral_shp folder has a bunch of different things (such as parcel data, easements)
+
+
